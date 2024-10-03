225c922c7b22a2d51881a6bd30ee73f96a1fd6e6
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2024-10-03 18:04:26 -0400

    adde recdutcion to handle add question and answer

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,4 @@
-import { RECEIVE_QUESTIONS } from "../actions/questions";
+import { RECEIVE_QUESTIONS, ADD_QUESTION, ADD_QUESTION_ANSWER } from "../actions/questions";
 
 export default function questions(state = {}, action) {
     switch(action.type) {
@@ -6,6 +6,22 @@ export default function questions(state = {}, action) {
             return {
                 ...state,
                 ...action.questions,
+            }
+        case ADD_QUESTION:
+            return {
+                ...state,
+                [action.question.id]: action.question,
+            }
+        
+        case ADD_QUESTION_ANSWER:
+            return {
+                ...state,
+                ...[action.id],
+                [action.answer] : {
+                    ...state[action.id][action.answer],
+                    votes: state[action.id][action.answer].text.conact([action.authedUser])
+
+                }
             }
         default:
             return state;
