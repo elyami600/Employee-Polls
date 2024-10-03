5ac1eb474d8c480c4a00670c37e2a619690c2441
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2024-10-03 18:02:03 -0400

    add actions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,4 +1,9 @@
-export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS "
+import { saveQuestion , saveQuestionAnswer } from "../utils/api";
+
+export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS"
+export const ADD_QUESTION      = "ADD_QUESTION"
+export const ADD_QUESTION_ANSWER = "ADD_QUESTION_ANSWER"
+
 
 export function receiveQuestions(questions) {
     return {
@@ -6,3 +11,72 @@ export function receiveQuestions(questions) {
       questions,
     };
   }
+// add question
+  export function addQuestion({ id, authedUser, answer }) {
+    return {
+      type: ADD_QUESTION,
+      id,
+      authedUser,
+      answer
+    }
+
+  }
+
+  export function handleAddQuestion(optionOne, optionTwo) {
+    return (dispatch, getState) => {
+      const { authedUser } = getState();
+      const question = { optionOne, optionTwo, authedUser }
+      dispatch(addQuestion(question));
+    
+      return saveQuestion(question).catch((e) => {
+        console.warn("Error in handleAddQuestion" ,e);
+        dispatch(addQuestion(question));
+        alert('There was an error saving the guestion. Try againg')
+      })
+
+    }
+
+  }
+//save the answer 
+  export function addQuestionAnswer({ authedUser, qid, answer }) {
+    return {
+      type: ADD_QUESTION_ANSWER,
+      authedUser,
+       qid, 
+       answer
+    }
+
+  }
+
+  export function handleAddQuestionAnswer({ qid, answer} ) {
+    return (dispatch, getState) => {
+      const { authedUser } = getState();
+
+      // dispatch(showLoading());
+
+      return saveQuestionAnswer({
+        authedUser,
+        qid,
+        answer
+      }).then(() => {
+        dispatch(addQuestionAnswer({authedUser, qid, answer}))
+      })
+      //.then(() => dispatch(hideLoading()));
+    }
+
+  }
+
+  export function handleAddAnswer(qid, answer) {
+    return (dispatch, getState) => {
+        const { authedUser } = getState();
+
+        dispatch(showLoading());
+
+        return saveQuestionAnswer(authedUser, qid, answer)
+        .then(() => {
+            dispatch(addAnswer(authedUser, qid, answer));
+            dispatch(addAnswerUser(authedUser, qid, answer));
+        })
+        .then(() => dispatch(hideLoading()));
+    };
+}
