3c1ab9f426151763c5af8fef4d98da8a97c71a35
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2024-10-09 13:39:11 -0400

    added reducer for the users

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,11 +1,31 @@
-import { RECEIVE_USERS } from "../actions/users";
+
+import { RECEIVE_USERS , ADD_USER_ANSWER, ADD_USER_QUESTION} from "../actions/users";
 
 export default function users(state = {}, action) {
     switch(action.type) {
         case RECEIVE_USERS:
             return {
                 ...state,
-                ...action.users
+                ...action.users,
+            };
+        case ADD_USER_QUESTION:
+            return {
+                ...state,
+                [action.author]: {
+                    ...state[action.author],
+                    questions: state[action.author].questions.concat(action.qid)
+                }      
+            }
+        case ADD_USER_ANSWER:
+            return {
+                ...state,
+                [action.authedUser]: {
+                    ...state[action.authedUser],
+                    answers: {
+                        ...state[action.authedUser].answers,
+                        [action.qid]: action.answer
+                    }
+                }
             };
         default:
             return state;
