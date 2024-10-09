80424dafde68c10b171609d4b84b7281e1da2c5d
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2024-10-09 15:05:33 -0400

    Updated  and fixed bugs

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,4 +1,6 @@
 import { saveQuestion , saveQuestionAnswer } from "../utils/api";
+import {addUserAnswer,addUserQuestion } from "./users";
+
 
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS"
 export const ADD_QUESTION      = "ADD_QUESTION"
@@ -12,56 +14,54 @@ export function receiveQuestions(questions) {
     };
   }
 // add question
-  export function addQuestion({ id, authedUser, answer }) {
-    return {
-      type: ADD_QUESTION,
-      id,
-      authedUser,
-      answer
-    }
-
+export function addQuestion({ id, authedUser, answer }) {
+  return {
+    type: ADD_QUESTION,
+    id,
+    authedUser,
+    answer
   }
 
-  export function handleAddQuestion(optionOne, optionTwo) {
-    return (dispatch, getState) => {
-      const { authedUser } = getState();
-      const question = { optionOne, optionTwo, authedUser }
+}
+
+export function handleAddQuestion(optionOne, optionTwo) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+    const question = { optionOne, optionTwo, authedUser }
+    dispatch(addQuestion(question));
+  
+    return saveQuestion(question).catch((e) => {
+      console.warn("Error in handleAddQuestion" ,e);
       dispatch(addQuestion(question));
+      dispatch(addUserQuestion(question))
+      alert('There was an error saving the guestion. Try againg')
+    })
+
+  }
+
+}
+// -------------------save the answe fun -------------------------//
+export function addQuestionAnswer(authedUser, qid, answer) {
+  return {
+    type: ADD_QUESTION_ANSWER,
+    authedUser,
+    qid, 
+      answer
+  }
+}
+
+
+export function handleAddQuestionAnswer(qid, answer) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+    // dispatch(showLoading());
     
-      return saveQuestion(question).catch((e) => {
-        console.warn("Error in handleAddQuestion" ,e);
-        dispatch(addQuestion(question));
-        alert('There was an error saving the guestion. Try againg')
-      })
-
-    }
-
-  }
-//save the answer 
-  export function addQuestionAnswer({ authedUser, qid, answer }) {
-    return {
-      type: ADD_QUESTION_ANSWER,
-      authedUser,
-       qid, 
-       answer
-    }
-
-  }
-
-  export function handleAddQuestionAnswer({ qid, answer} ) {
-    return (dispatch, getState) => {
-      const { authedUser } = getState();
-
-      // dispatch(showLoading());
-
-      return saveQuestionAnswer({
-        authedUser,
-        qid,
-        answer
-      }).then(() => {
-        dispatch(addQuestionAnswer({authedUser, qid, answer}))
-      })
+    return saveQuestionAnswer({ authedUser, qid, answer })
+    .then(() => {
+      dispatch(addQuestionAnswer(authedUser, qid, answer))
+      dispatch(addUserAnswer(authedUser, qid, answer))
+    })
       //.then(() => dispatch(hideLoading()));
-    }
-
   }
+
+}
