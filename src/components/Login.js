853568a4df3f0f2c9b46fd16e66a38db99bdeb58
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2024-10-12 00:49:19 -0400

    added a login page to app and

---
+++ src/components/Login.js
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import { connect } from "react-redux";
+import { setAuthedUser } from "../actions/authedUser";
+import { useNavigate } from "react-router";
+
+
+
+
+const LogingPage = ({  dispatch , users }) => {
+    console.log("Longing page ", users)
+    const navigate = useNavigate()
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsername= (e) => {
+        e.preventDefault()
+        const username = e.target.value;
+       setUsername(username)
+    }
+
+    const handlePassword = (e) => {
+        e.preventDefault()
+        const password = e.target.value;
+        setPassword(password)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const userExit = users.find((user) => 
+            (user.id.includes(username) && 
+            user.password.includes(password)))  
+
+        if(userExit) {
+            dispatch(setAuthedUser(username)); 
+            setSuccess(true)
+            setError(false)
+            navigate("/");
+            return;
+        } 
+        setSuccess(false);
+        setError(true);
+        setUsername("")
+        setPassword("")
+
+    }
+
+    return(
+        <div className="container">
+            {error &&
+                <h1 data-testid="error-header">Error: Please ensure all fields are filled out.</h1>
+            }
+            <h1 className="center">Long In</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>username:</label>
+                    <input
+                        data-testid="email-input"
+                        type="text"
+                        placeholder="username..."
+                        value={username}
+                        onChange={handleUsername}
+                    />
+                </div>
+                <div>
+                    <label>password:</label>
+                    <input
+                        data-testid="email-input"
+                        type="text"
+                        placeholder="password..."
+                        value={password}
+                        onChange={handlePassword}
+                    />
+                </div>
+                <input
+                    data-testid="submit-button"
+                    type="submit"
+                    value="Submit"
+                />
+            </form>
+            
+        </div>
+    )
+}
+const mapStateToProps = ({ users }) => {
+    return { 
+        //users,
+      
+       users : Object.values(users),
+     }
+}
+export default connect(mapStateToProps)(LogingPage);
