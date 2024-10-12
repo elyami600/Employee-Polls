b603d8dc068df672038e3b9f95cebc257e2e1a71
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2024-10-12 00:50:53 -0400

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import { handleInitialData } from "../actions/shared";
 import { connect } from "react-redux";
 import Dashboard from "./Dashboard";
-
-
+import Nav from "./Nav";
+import { Routes, Route } from "react-router";
+import NewQuestion from "./NewQuestion";
+import LeaderBoard from "./LeaderBoard";
+import NotFound from "./404page"
+import Login from "./Login";
+import PollAnswer from "./PollAnswer";
 
 
 
@@ -13,14 +18,31 @@ const App = (props) => {
 
    useEffect(() => {
       props.dispatch(handleInitialData());
-    }, [])
-
-
+    }, []);
+  
   return (
-   <div>{props.loading === true ? null : <Dashboard />}</div>
+    <div>
+      <Fragment>
+        <div className="container"></div>
+        <Nav/>
+        {
+        props.loading === true ? null : (
+          <Routes>
+             <Route path="/" exact element={<Dashboard/>}/>
+             <Route path="/question/:id" element={<PollAnswer/>}/>
+             <Route path="/leaderdoard" element={<LeaderBoard/>}/>
+             <Route path="/add" element={<NewQuestion/>}/>
+             <Route path="/login" element={<Login/>}/>
+             <Route path="*" element={<NotFound/>} />
+          </Routes>
+        )}
+      </Fragment>
+    </div>
   );
 }
+
 const mapStateToProps = ({ authedUser }) => ({
   loading: authedUser === null,
-});
-export default connect(mapStateToProps)(App);
+})
+
+export default  connect(mapStateToProps)(App);
