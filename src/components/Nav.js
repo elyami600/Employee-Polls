d06340cd5940959d147a4d595ed3277aae143c4a
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2024-10-17 16:24:01 -0400

    update  te component test id and added user who is login which will show the id or name

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,47 @@
-import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { setAuthedUser } from "../actions/authedUser";
 
-const Nav = () => {
+
+const Nav = ({ dispatch, authedUser }) => {
+    const navigate = useNavigate();
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(setAuthedUser(null));
+        navigate('/login');
+      };
+
     return (
         <nav className="nav">
             <ul>
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link data-testid='home' to="/">Home</Link>
                 </li>
                 <li>
-                <Link to="/leaderdoard">Leaderdboard</Link>
+                    <Link data-testid='leaderdoard' to="/leaderdoard">Leaderdboard</Link>
                 </li>
                 <li>
-                <Link to="/add">New Question</Link>
+                    <Link data-testid='newQuestion' to="/add">New Question</Link>
                 </li>
                 <li>
-                <Link to="/login">Loging</Link>
+                    <Link data-testid='loging'      to="/login">Loging</Link>
                 </li>
+                <li data-testid='authedUser'>
+                     User: {authedUser}
+                </li>
+                {authedUser !== null && <li>
+                    <Link data-testid='logout' onClick={logout}>Logout</Link>
+                </li>}
             </ul>
 
         </nav>
     )
 }
-export default Nav;
+
+const mapStateToProps = ({ authedUser }) => {
+    return {
+      authedUser,
+    };
+  };
+export default connect(mapStateToProps)(Nav);
