315fd46837c0777e3fe0e1028768339e90b364aa
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2024-10-24 01:53:32 -0400

    updated fuction to check if user answer or not the question and display

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,22 @@
 import { connect } from "react-redux";
 import Question from "./Question";
 
-const Dashboard = (props) => {
-    console.log("Dashboard props ", props)
+const Dashboard = ({questions, questionsIds, authedUser }) => {
+
+  // check if the id has answer the question
+  // return new array questionID that authedUser hasnt voter yet
+  const hasAnwerQuestion = questionsIds.filter((id) => {
+    const { optionOne, optionTwo } = questions[id];
+    return optionOne.votes.includes(authedUser) || optionTwo.votes.includes(authedUser);
+  });
+  const notAnswersQuestion = questionsIds.filter((id) => !hasAnwerQuestion.includes(id));
+  
+  console.log("Dashboard props ", questionsIds)
     return (
       <div className="card-grid">
         <h3 className="center">New Questions</h3>
         <ul className="dashboard-list">
-            {props.questionsIds.map((id) =>(
+            {notAnswersQuestion.map((id) =>(
                 <li key={id}>
                   <Question id={id}/>
                 </li>
@@ -17,9 +26,14 @@ const Dashboard = (props) => {
     )
 }
 
-const mapStateToProps = ({ questions }) => ({
+const mapStateToProps = ({ questions, authedUser}) => ({
+
     questionsIds: Object.keys(questions).sort(
       (a, b) => questions[b].timestamp - questions[a].timestamp
     ),
+    questions,
+    authedUser,
+  
+  
   });
 export default connect(mapStateToProps)(Dashboard);
