cca5bfdc2cb6d2556450a424f5cf804299994b2e
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2024-10-24 12:19:53 -0400

    adder a blog to show all the questionn the user voted

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,7 +4,7 @@ import Question from "./Question";
 const Dashboard = ({questions, questionsIds, authedUser }) => {
 
   // check if the id has answer the question
-  // return new array questionID that authedUser hasnt voter yet
+  // return new array question ID that authedUser hasnt voted yet
   const hasAnwerQuestion = questionsIds.filter((id) => {
     const { optionOne, optionTwo } = questions[id];
     return optionOne.votes.includes(authedUser) || optionTwo.votes.includes(authedUser);
@@ -17,6 +17,17 @@ const Dashboard = ({questions, questionsIds, authedUser }) => {
         <h3 className="center">New Questions</h3>
         <ul className="dashboard-list">
             {notAnswersQuestion.map((id) =>(
+                <li key={id}>
+                  <Question id={id}/>
+                </li>
+            ))}
+        </ul>
+
+        <hr style={{ border:"1px dotted #8c8b8b", width: "700px", color: 'blue', fontSize: '20px' }}></hr>
+
+        <h3 className="center">Done</h3>
+        <ul className="dashboard-list">
+            {questionsIds.map((id) =>(
                 <li key={id}>
                   <Question id={id}/>
                 </li>
