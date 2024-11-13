50108fcc2f3ef62ce4dc79d1b8f35f98baa98dda
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2024-11-13 14:35:09 -0500

    updates snapshots

---
+++ src/utils/App.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import App from "../components/App"
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import reducer from "../reducers";
+import middleware from "../middleware";
+import { createStore } from "redux";
+import { render } from "@testing-library/react";
+
+const store = createStore(reducer, middleware);
+
+describe("will test the componensts", () => {
+  it("should be render  the App component correctly", () => {
+      const component = render(
+          <Provider store={store}>
+              <Router>
+                  <App/>
+              </Router>
+          </Provider>
+      );
+      expect(component).toBeDefined();
+      expect(component).toMatchSnapshot();
+
+  });
+});
